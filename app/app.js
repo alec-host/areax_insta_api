6eee2704a02d38f46170539e76c84d123ee3e4e5
alec-host <irualec@yahoo.com>
2024-05-22 23:57:16 +0300

    4th commit: added npm library: image-size,modified post.controller.js,post.routes.js and .gitignore

--- app/app.js
+++ app/app.js
@@ -1,3 +1,5 @@
+const cors = require("cors");
+const morgan = require("morgan");
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/post.routes');
@@ -8,10 +10,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
-app.use('/messaging', routes);
+app.use('/instagram/messaging', routes);
 
 app.use(error.errorHandler)
 
--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const multer = require('multer');
-const { postCaptionWithImage } = require('../controllers/post.controller');
+const { postPing, postCaptionWithImage } = require('../controllers/post.controller');
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
+router.post('/ping', postPing);
 router.post('/caption-image', upload.single('image'), postCaptionWithImage);
 
 module.exports = router;
